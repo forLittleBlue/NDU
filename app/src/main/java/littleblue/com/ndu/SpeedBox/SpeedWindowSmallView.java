b59da52dd9178599ea80c32bbdc8304109a43972
aLittleBlue <364740623@qq.com>
2016-11-09 09:10:06 +0800

    Add speedBox

---
+++ app/src/main/java/littleblue/com/ndu/SpeedBox/SpeedWindowSmallView.java
@@ -0,0 +1,15 @@
+package littleblue.com.ndu.SpeedBox;
+
+import android.view.MotionEvent;
+import android.widget.LinearLayout;
+
+/**
+ * Created by 10964492 on 2016/11/9.
+ */
+
+public class SpeedWindowSmallView extends LinearLayout {
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+}
